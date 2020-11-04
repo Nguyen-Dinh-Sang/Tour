e56632399faf3c5a8395d9a3b342d6bce10b0920
DinhSang <dinhsangdroid@gmail.com>
2020-11-05 02:50:18 +0700

    DiaDiem-DinhSang: Còn vài lỗi nhỏ

--- Code/TourMVC/TourMVC/Models/TourDiaDiem.cs
+++ Code/TourMVC/TourMVC/Models/TourDiaDiem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace TourMVC.Models
 {
@@ -11,9 +12,14 @@ namespace TourMVC.Models
         }
 
         public int DiaDiemId { get; set; }
+        [Display(Name = "Thành Phố")]
         public string DiaDiemThanhPho { get; set; }
+        [Display(Name = "Tên Địa Điểm")]
         public string DiaDiemTen { get; set; }
+        [Display(Name = "Mô Tả")]
         public string DiaDiemMoTa { get; set; }
+        [Display(Name = "Ngày Tạo")]
+        [DataType(DataType.Date)]
         public DateTime? NgayTao { get; set; }
 
         public virtual ICollection<TourChiTiet> TourChiTiet { get; set; }
