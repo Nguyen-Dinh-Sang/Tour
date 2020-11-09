6fcb085c3e2738f5ddde712b0adea0f34e8b9ac7
DinhSang <dinhsangdroid@gmail.com>
2020-11-09 09:11:51 +0700

    LoaiChiPhi-DinhSang: Vietsub

--- Code/TourMVC/TourMVC/Models/TourLoaiChiPhi.cs
+++ Code/TourMVC/TourMVC/Models/TourLoaiChiPhi.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace TourMVC.Models
 {
@@ -11,8 +12,12 @@ namespace TourMVC.Models
         }
 
         public int LoaiChiPhiId { get; set; }
+        [Display(Name = "Loại Chi Phí")]
         public string LoaiChiPhiTen { get; set; }
+        [Display(Name = "Mô Tả")]
         public string LoaiChiPhiMoTa { get; set; }
+        [Display(Name = "Ngày Tạo")]
+        [DataType(DataType.Date)]
         public DateTime? NgayTao { get; set; }
 
         public virtual ICollection<TourChiPhiChiTiet> TourChiPhiChiTiet { get; set; }
