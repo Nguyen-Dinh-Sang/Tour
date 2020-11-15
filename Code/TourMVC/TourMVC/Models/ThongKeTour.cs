79bb9156a00cccbe85e2ae87a6b35d1452ab790c
DinhSang <dinhsangdroid@gmail.com>
2020-11-15 21:02:41 +0700

    Fix-DinhSang: Còn nhiều bug lắm

---
+++ Code/TourMVC/TourMVC/Models/ThongKeTour.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TourMVC.Models
+{
+    public partial class ThongKeTour
+    {
+        public ThongKeTour()
+        {
+        }
+
+        public int TourId { get; set; }
+        [Display(Name = "Tour")]
+        public string TourTen { get; set; }
+
+        [Display(Name = "Tổng Số Đoàn Đi")]
+        public int TongSoDoan { get; set; }
+
+        [Display(Name = "Tổng Doanh Thu")]
+        public decimal TongDoanhThu { get; set; }
+
+        [Display(Name = "Tổng Chi Phí")]
+        public decimal TongChiPhi { get; set; }
+
+        [Display(Name = "Tổng Lợi Nhuận")]
+        public decimal TongLoiNhuan { get; set; }
+    }
+}
