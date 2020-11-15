72a823862284b08f093e7df0deb6988308bc7621
DinhSang <dinhsangdroid@gmail.com>
2020-11-16 04:20:26 +0700

    Fix-DinhSang: Địa điểm du lịch chốt

--- Code/TourMVC/TourMVC/Models/TourDiaDiem.cs
+++ Code/TourMVC/TourMVC/Models/TourDiaDiem.cs
@@ -13,10 +13,13 @@ namespace TourMVC.Models
 
         public int DiaDiemId { get; set; }
         [Display(Name = "Thành Phố")]
+        [Required(ErrorMessage = "Thành Phố Không Được Để Trống")]
         public string DiaDiemThanhPho { get; set; }
         [Display(Name = "Tên Địa Điểm")]
+        [Required(ErrorMessage = "Địa Điểm Không Được Để Trống")]
         public string DiaDiemTen { get; set; }
         [Display(Name = "Mô Tả")]
+        [Required(ErrorMessage = "Mô Tả Không Được Để Trống")]
         public string DiaDiemMoTa { get; set; }
         [Display(Name = "Ngày Tạo")]
         [DataType(DataType.Date)]
