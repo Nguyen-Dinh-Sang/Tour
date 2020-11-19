23c9111b0750a42ecbbcacc5df9e0035cc3288a7
DinhSang <dinhsangdroid@gmail.com>
2020-11-19 22:51:54 +0700

    Fix-DinhSang: Xong

--- Code/TourMVC/TourMVC/Controllers/ThongKeController.cs
+++ Code/TourMVC/TourMVC/Controllers/ThongKeController.cs
@@ -21,60 +21,244 @@ namespace TourMVC.Controllers
 
         public IActionResult Index()
         {
-            var query = (from t in context.Tour
-                         join d in (
-                             (from td in context.TourDoan
-                              join dkh in context.DoanKhachHang on td.DoanId equals dkh.DoanId into dkh_join
-                              from dkh in dkh_join.DefaultIfEmpty()
-                              join cpct in context.TourChiPhiChiTiet on td.DoanId equals cpct.DoanId into cpct_join
-                              from cpct in cpct_join.DefaultIfEmpty()
-                              group new { td, dkh, cpct } by new
-                              {
-                                  td.TourId,
-                                  td.DoanId,
-                                  td.DoanGiaTour
-                              } into g
-                              select new
-                              {
-                                  g.Key.TourId,
-                                  Doan_ID = (int?)g.Key.DoanId,
-                                  SoLuong_Khach = g.Count(p => p.dkh.DoanKhachHangId != null),
-                                  DoanhThu = (decimal?)(g.Count(p => p.dkh.DoanKhachHangId != null) * g.Key.DoanGiaTour),
-                                  ChiPhi = (decimal?)g.Sum(p => p.cpct.ChiPhi)
-                              })) on new { Tour_ID = t.TourId } equals new { Tour_ID = Convert.ToInt32(d.TourId) } into d_join
-                         from d in d_join.DefaultIfEmpty()
-                         group new { t, d } by new
-                         {
-                             t.TourId,
-                             t.TourTen
-                         } into g
-                         select new
-                         {
-                             g.Key.TourId,
-                             g.Key.TourTen,
-                             Tong_SoDoan = g.Count(p => p.d.Doan_ID != null),
-                             Tong_DoanhThu = (decimal?)g.Sum(p => p.d.DoanhThu),
-                             Tong_ChiPhi = (decimal?)g.Sum(p => p.d.ChiPhi)
-                         }).AsEnumerable();
 
+            var queryTour_Doan = (
+                from t in context.Tour
+                join td in context.TourDoan on t.TourId equals td.TourId into t_td_join
+                from t_td in t_td_join.DefaultIfEmpty()
+                select new
+                {
+                    TourId = t.TourId,
+                    TourTen = t.TourTen,
+                    DoanId = t_td.DoanId,
+                    NgayTao = t_td.NgayTao,
+                    Gia = t_td.DoanGiaTour
+                }
+                );
 
-            
+            // lọc khúc này là ổn rồi
 
-            foreach (var item in query)
-            {
-                Console.WriteLine($"{item.TourId + ":",-15}{item.TourTen + ":",-15}");
-            }
-            return View();
+            var queryDoanKhachHang = (
+                    from dkh in context.DoanKhachHang
+                    group new { dkh } by new
+                    {
+                        dkh.DoanId
+                    } into g
+                    select new
+                    {
+                        DoanId = g.Key.DoanId,
+                        SoLuongKhach = g.Count()
+                    }
+                );
+
+            var queryChiPhi = (
+                    from cp in context.TourChiPhiChiTiet
+                    group new { cp } by new
+                    {
+                        cp.DoanId
+                    } into g
+                    select new
+                    {
+                        DoanId = g.Key.DoanId,
+                        TongChiPhi = (decimal?)g.Sum(cp => cp.cp.ChiPhi)
+                    }
+                );
+
+            var queryDoan_KhachHang = (
+                    from td in queryTour_Doan
+                    join kh in queryDoanKhachHang on td.DoanId equals kh.DoanId into td_kh_join
+                    from td_kh in td_kh_join.DefaultIfEmpty()
+                    select new
+                    {
+
+                        TourId = td.TourId,
+                        TourTen = td.TourTen,
+                        DoanId = td.DoanId,
+                        Gia = td.Gia,
+                        SoLuongKhach = td_kh.SoLuongKhach
+                    }
+                );
+
+            var queryDoan_KhachHang_ChiPhi = (
+                    from tdkh in queryDoan_KhachHang
+                    join cp in queryChiPhi on tdkh.DoanId equals cp.DoanId into tdkh_cp_join
+                    from tdkh_cp in tdkh_cp_join.DefaultIfEmpty()
+                    select new
+                    {
+                        TourId = tdkh.TourId,
+                        TourTen = tdkh.TourTen,
+                        DoanId = tdkh.DoanId,
+                        Gia = tdkh.Gia,
+                        SoLuongKhach = tdkh.SoLuongKhach,
+                        TongChiPhi = tdkh_cp.TongChiPhi
+                    }
+                );
+
+            var queryDoan_KhachHang_ChiPhi_DoanhThu = (
+                from dkhcp in queryDoan_KhachHang_ChiPhi
+                select new
+                {
+                    TourId = dkhcp.TourId,
+                    TourTen = dkhcp.TourTen,
+                    DoanId = dkhcp.DoanId,
+                    TongDoanhThu = (decimal?)(dkhcp.Gia * dkhcp.SoLuongKhach),
+                    TongChiPhi = dkhcp.TongChiPhi
+                }
+                );
+
+            var queryTour_DoanhThu_ChiPhi = (
+                from dkhcpdt in queryDoan_KhachHang_ChiPhi_DoanhThu
+                where dkhcpdt.DoanId > 0
+                group new { dkhcpdt } by new
+                {
+                    dkhcpdt.TourId
+                } into g
+                select new
+                {
+                    TourId = g.Key.TourId,
+                    TongDoanhThu = (decimal?)g.Sum(dkhcpdt => dkhcpdt.dkhcpdt.TongDoanhThu),
+                    TongChiPhi = (decimal?)g.Sum(dkhcpdt => dkhcpdt.dkhcpdt.TongChiPhi),
+                    TongSoDoan = g.Count()
+                }
+                );
+
+            var queryCuoiCung = (
+                    from t in context.Tour
+                    join tdtcp in queryTour_DoanhThu_ChiPhi on t.TourId equals tdtcp.TourId into t_tdtcp_join
+                    from t_tdtcp in t_tdtcp_join.DefaultIfEmpty()
+                    select new ThongKeTour
+                    {
+                        TourId = t.TourId,
+                        TourTen = t.TourTen,
+                        TongSoDoan = t_tdtcp.TongSoDoan == null ? 0 : t_tdtcp.TongSoDoan,
+                        TongDoanhThu = t_tdtcp.TongDoanhThu == null ? 0 : t_tdtcp.TongDoanhThu,
+                        TongChiPhi = t_tdtcp.TongChiPhi == null ? 0 : t_tdtcp.TongChiPhi
+                    }
+                );
+
+            var tongDoanhThu = queryCuoiCung.Sum(i => i.TongDoanhThu);
+            var tongChiPhi = queryCuoiCung.Sum(i => i.TongChiPhi); 
+
+            ViewData["TongSoDoan"] = queryCuoiCung.Sum(i => i.TongSoDoan);
+            ViewData["TongDoanhThu"] = tongDoanhThu;
+            ViewData["TongChiPhi"] = tongChiPhi;
+            ViewData["TongLoiNhuan"] = (tongDoanhThu - tongChiPhi);
+
+            return View(queryCuoiCung);
         }
 
-        private object coalesce(decimal? v1, int v2)
+        public async Task<IActionResult> Details(int? id)
         {
-            if (v1 == null)
+            if (id == null)
             {
-                return v2;
+                return NotFound();
             }
 
-            return v1;
+            var tour = await context.Tour.FindAsync(id);
+            ViewData["Tour"] = tour.TourTen;
+
+            var queryDoan = (
+                from d in context.TourDoan
+                where d.TourId == id
+                select new
+                {
+                    DoanId = d.DoanId,
+                    DoanTen = d.DoanTen,
+                    NgayTao = d.NgayTao,
+                    GiaTien = d.DoanGiaTour
+                }
+                );
+
+            // lọc khúc này là ổn rồi
+
+            var queryDoanKhachHang = (
+                    from dkh in context.DoanKhachHang
+                    group new { dkh } by new
+                    {
+                        dkh.DoanId
+                    } into g
+                    select new
+                    {
+                        DoanId = g.Key.DoanId,
+                        SoLuongKhach = g.Count()
+                    }
+                );
+
+            var queryChiPhi = (
+                    from cp in context.TourChiPhiChiTiet
+                    group new { cp } by new
+                    {
+                        cp.DoanId
+                    } into g
+                    select new
+                    {
+                        DoanId = g.Key.DoanId,
+                        TongChiPhi = (decimal?)g.Sum(cp => cp.cp.ChiPhi)
+                    }
+                );
+
+            var queryDoan_KhachHang = (
+                    from td in queryDoan
+                    join kh in queryDoanKhachHang on td.DoanId equals kh.DoanId into td_kh_join
+                    from td_kh in td_kh_join.DefaultIfEmpty()
+                    select new
+                    {
+
+                        DoanId = td.DoanId,
+                        DoanTen = td.DoanTen,
+                        GiaTien = td.GiaTien,
+                        SoLuongKhach = td_kh.SoLuongKhach
+                    }
+                );
+
+            var queryCuoiCung = (
+                   from tdkh in queryDoan_KhachHang
+                   join cp in queryChiPhi on tdkh.DoanId equals cp.DoanId into tdkh_cp_join
+                   from tdkh_cp in tdkh_cp_join.DefaultIfEmpty()
+                   select new ThongKeDoan
+                   {
+                       DoanId = tdkh.DoanId,
+                       DoanTen = tdkh.DoanTen,
+                       SoLuongKhach = tdkh.SoLuongKhach,
+                       GiaTien = tdkh.GiaTien,
+                       TongDoanhThu = (decimal?)(tdkh.GiaTien * tdkh.SoLuongKhach) == null ? 0 : (decimal?)(tdkh.GiaTien * tdkh.SoLuongKhach),
+                       TongChiPhi = tdkh_cp.TongChiPhi == null ? 0 : tdkh_cp.TongChiPhi
+                   }
+               );
+
+            var tongDoanhThu = queryCuoiCung.Sum(i => i.TongDoanhThu);
+            var tongChiPhi = queryCuoiCung.Sum(i => i.TongChiPhi);
+
+            ViewData["TongSoKhach"] = queryCuoiCung.Sum(i => i.SoLuongKhach);
+            ViewData["TongDoanhThu"] = tongDoanhThu;
+            ViewData["TongChiPhi"] = tongChiPhi;
+            ViewData["TongLoiNhuan"] = (tongDoanhThu - tongChiPhi);
+
+            return View(queryCuoiCung);
+        }
+
+        public async Task<IActionResult> DetailsDoan(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var tourDoan = await context.TourDoan
+                .Include(t => t.Tour)
+                .FirstOrDefaultAsync(m => m.DoanId == id);
+            context.Entry(tourDoan).Collection(td => td.DoanKhachHang).Query().Include(dkh => dkh.KhachHang).OrderBy(dkh => dkh.NgayTao).Load();
+            context.Entry(tourDoan).Collection(td => td.DoanNhanVien).Query().Include(dnv => dnv.NhanVien).OrderBy(dnv => dnv.NgayTao).Load();
+            context.Entry(tourDoan).Collection(td => td.TourChiPhiChiTiet).Query().Include(tcpct => tcpct.LoaiChiPhi).OrderBy(tcpct => tcpct.NgayTao).Load();
+
+            ViewData["TongChiPhi"] = context.TourChiPhiChiTiet.Where(tcpct => tcpct.DoanId == id).Sum(i => i.ChiPhi);
+
+            if (tourDoan == null)
+            {
+                return NotFound();
+            }
+
+            return View(tourDoan);
         }
     }
 }
